f3ec709d024bd503b5f73005c8f868b11207404b
Alexander Baquiax <alex@yalo.com>
2023-08-25 20:59:28 -0600

    feat: adding textboxes

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+# Import necessary libraries
 import streamlit as st
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+
+def main():
+    st.title("Desing could be found here: https://excalidraw.com/#room=5a1eb83a9c0ba4ee278b,j6-eKhrIdcLkM9BnRxtAgg")
+
+    # Get user input for error percentage
+    error_percentage = st.text_input("Enter Error Percentage:")
+
+    # Get user input for amount of iterations
+    iterations = st.text_input("Enter Amount of Iterations:")
+
+    # Button to display the inputs
+    if st.button("Submit"):
+        # Basic input validation
+        try:
+            error = float(error_percentage)
+            iters = int(iterations)
+
+            st.write(f"You entered {error}% error percentage.")
+            st.write(f"You entered {iters} iterations.")
+        except ValueError:
+            st.write("Please enter valid values.")
+
+if __name__ == '__main__':
+    main()
