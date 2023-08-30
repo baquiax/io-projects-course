2b9949a3a0db630fc92809e4b7dd11306ce19ca9
Alexander Baquiax <alex@yalo.com>
2023-08-29 18:59:04 -0600

    feat: learning curve calculator

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 # Import necessary libraries
 import streamlit as st
+import matplotlib.pyplot as plt
+import math 
 
 st.title("Curvas de aprendizaje y Simulación Montecarlo")
 st.write("[Ver el diseño del a aplicación](https://excalidraw.com/#room=5a1eb83a9c0ba4ee278b,j6-eKhrIdcLkM9BnRxtAgg)")
 
-learning_tab, montecarlo_tab = st.tabs(["Curvas de aprendizaje", "Simulación Montecarlo"])
+learning_tab, montecarlo_tab = st.tabs(["📉  Curvas de aprendizaje", "⏰  Simulación Montecarlo"])
 with learning_tab:
-    # Get user input for error percentage
-    error_percentage = st.text_input("Enter Error Percentage:")
+    p = st.number_input(label="% de aprendizaje", value=80.0, min_value=1.0, max_value=99.999)
 
-    # Get user input for amount of iterations
-    iterations = st.text_input("Enter Amount of Iterations:")
+    a = st.number_input(label="Tiempo para la primera unidad", value=800, min_value=1)
 
+    X = st.number_input(label="Unidades requeridas",  value=16, min_value=2)
+    
+    # text_input(label="Unidades requeridas", value=16)
+    
     # Button to display the inputs
-    if st.button("Submit"):
+    
         # Basic input validation
-        try:
-            error = float(error_percentage)
-            iters = int(iterations)
+    try:
+        p = float(p) / 100
+        a = float(a)
+        X = int(X)
+        b = math.log(p) / math.log(2)
+        
+        y = a * math.pow(X, b)
+        
+        st.success(f"Cantidad de tiempo para producir **{X}** unidades: **{y:.2f}**", icon="📈")
+        
+        x_values = list(range(1, X + 1))
+        y_values = [a * math.pow(x, b) for x in x_values]
 
-            st.write(f"You entered {error}% error percentage.")
-            st.write(f"You entered {iters} iterations.")
-        except ValueError:
-            st.write("Please enter valid values.")
+
+        fig, ax = plt.subplots()
+        ax.plot(x_values, y_values)
+        
+        ax.set_xlabel('Unidades')
+        ax.set_ylabel('Tiempo')
+
+        # Display the plot in the Streamlit app
+        st.pyplot(fig)
+
+    except ValueError as error:
+        st.write("Please enter valid values.")
+        st.write(error)
 
 with montecarlo_tab:
     # Get user input for error percentage
